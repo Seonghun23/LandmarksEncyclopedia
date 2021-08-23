325061bed24da52334e585eeefb85492ce99b19e
Seonghun Kim <kim.seonghun23@gmail.com>
2021-08-24 00:01:01 +0900

    Initial Commit

---
+++ LandmarksEncyclopedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarksEncyclopedia
+//
+//  Created by Seonghun Kim on 2021/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
